fd6f4d54e1e9f0776400fd58ab481db5c5d104c2
udarakr <udarakr@gmail.com>
2016-09-16 12:43:06 +0530

    adding few validation

--- update.sh
+++ update.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-gitrepo="/home/udara/pidownloader/pidwnlist"
+baseDir="/home/udara/pidownloader/tmp/"
+gitrepoName="pidwnlist"
+gitDir="$baseDir$gitrepoName"
+echo "Git repository $gitDir"
+gitRepo="https://github.com/udarakr/pidwnlist.git"
 
 
-cd $gitrepo
-echo "Starting git update in $gitrepo"
-git pull
+if [ -d $gitDir ]
+then
+    echo "Directory $gitDir already exists."
+    cd $gitDir
+    echo "Starting git update in $gitDir"
+	git pull
+else
+	echo "Directory $gitDir not found."
+    mkdir -p $baseDir
+    cd $baseDir
+    echo "Cloning $gitRepo"
+    git clone $gitRepo
+fi
+
+
